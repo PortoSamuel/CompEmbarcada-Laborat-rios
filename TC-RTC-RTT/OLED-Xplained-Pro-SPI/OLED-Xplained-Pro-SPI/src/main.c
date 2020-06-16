aeb961fcf2713615fec597b27f4cb3e18f0269fd
PortoSamuel <samuelnnp@al.insper.edu.bbr>
2020-06-16 20:46:41 -0300

    Lab TC-RTT-RTC entrega A

--- TC-RTC-RTT/OLED-Xplained-Pro-SPI/OLED-Xplained-Pro-SPI/src/main.c
+++ TC-RTC-RTT/OLED-Xplained-Pro-SPI/OLED-Xplained-Pro-SPI/src/main.c
@@ -4,10 +4,14 @@
 #include "gfx_mono_text.h"
 #include "sysfont.h"
 
-#define LED_PIO_ID	    ID_PIOC
-#define LED_PIO         PIOC
-#define LED_PIN		      8
-#define LED_IDX_MASK    (1<<LED_PIN)
+/************************************************************************/
+/* DEFINES                                                              */
+/************************************************************************/
+
+#define LED_PIO_ID ID_PIOC
+#define LED_PIO PIOC
+#define LED_PIN 8
+#define LED_IDX_MASK (1 << LED_PIN)
 
 #define LED3_PLACA_PIO PIOB
 #define LED3_PLACA_PIO_ID ID_PIOB
@@ -38,12 +42,15 @@ typedef struct  {
 /* VAR globais                                                          */
 /************************************************************************/
 
-volatile char flag_tc = 0;
-
-volatile char flag_tc_2 = 0;
-
-volatile char flag_rtc = 0;
-
+uint32_t pul_hour;
+uint32_t pul_minute;
+uint32_t pul_second;
+char str[20];
+volatile char segundo = 0;
+volatile char flag_lad1 = 0;
+volatile char flag_lad2 = 0;
+volatile char flag_lad3 = 0;
+volatile char flag_led = 0;
 volatile Bool f_rtt_alarme = false;
 
 /************************************************************************/
@@ -51,12 +58,10 @@ volatile Bool f_rtt_alarme = false;
 /************************************************************************/
 
 void LED_init(int estado);
-void TC_init(Tc * TC, int ID_TC, int TC_CHANNEL, int freq);
-void TC_init_2(Tc * TC, int ID_TC, int TC_CHANNEL, int freq);
-void RTC_init(Rtc *rtc, uint32_t id_rtc, calendar t, uint32_t irq_type);
 void pin_toggle(Pio *pio, uint32_t mask);
-void io_init(void);
+void TC_init(Tc * TC, int ID_TC, int TC_CHANNEL, int freq);
 static void RTT_init(uint16_t pllPreScale, uint32_t IrqNPulses);
+void RTC_init(Rtc *rtc, uint32_t id_rtc, calendar t, uint32_t irq_type);
 
 /************************************************************************/
 /* Handlers                                                             */
@@ -84,47 +89,22 @@ void TC1_Handler(void){
 	UNUSED(ul_dummy);
 
 	/** Muda o estado do LED */
-	flag_tc = 1;
+	flag_lad1 = 1;
 }
 
-void TC1_Handler_2(void){
+void TC4_Handler(void){
 	volatile uint32_t ul_dummy;
 
 	/****************************************************************
 	* Devemos indicar ao TC que a interrupção foi satisfeita.
 	******************************************************************/
-	ul_dummy = tc_get_status(TC0, 1);
+	ul_dummy = tc_get_status(TC1, 1);
 
 	/* Avoid compiler warning */
 	UNUSED(ul_dummy);
 
 	/** Muda o estado do LED */
-	flag_tc_2 = 1;
-}
-
-void RTC_Handler(void)
-{
-	uint32_t ul_status = rtc_get_status(RTC);
-
-	/*
-	*  Verifica por qual motivo entrou
-	*  na interrupcao, se foi por segundo
-	*  ou Alarm
-	*/
-	if ((ul_status & RTC_SR_SEC) == RTC_SR_SEC) {
-		rtc_clear_status(RTC, RTC_SCCR_SECCLR);
-	}
-	
-	/* Time or date alarm */
-	if ((ul_status & RTC_SR_ALARM) == RTC_SR_ALARM) {
-			rtc_clear_status(RTC, RTC_SCCR_ALRCLR);
-      flag_rtc = 1;
-	}
-	
-	rtc_clear_status(RTC, RTC_SCCR_ACKCLR);
-	rtc_clear_status(RTC, RTC_SCCR_TIMCLR);
-	rtc_clear_status(RTC, RTC_SCCR_CALCLR);
-	rtc_clear_status(RTC, RTC_SCCR_TDERRCLR);
+	flag_lad2 = 1;
 }
 
 void RTT_Handler(void)
@@ -135,13 +115,50 @@ void RTT_Handler(void)
 	ul_status = rtt_get_status(RTT);
 
 	/* IRQ due to Time has changed */
-	if ((ul_status & RTT_SR_RTTINC) == RTT_SR_RTTINC) {  }
+	if ((ul_status & RTT_SR_RTTINC) == RTT_SR_RTTINC) {
+		//f_rtt_alarme = false;
+		//pin_toggle(LED_PIO, LED_IDX_MASK);    // BLINK Led
+		
+	}
 
 	/* IRQ due to Alarm */
 	if ((ul_status & RTT_SR_ALMS) == RTT_SR_ALMS) {
-		pin_toggle(LED2_PLACA_PIO, LED2_PLACA_IDX_MASK);    // BLINK Led
+		// pin_toggle(LED_PIO, LED_IDX_MASK);    // BLINK Led
 		f_rtt_alarme = true;                  // flag RTT alarme
 	}
+}
+
+/**
+ * \brief Interrupt handler for the RTC. 
+ */
+void RTC_Handler(void)
+{
+	uint32_t ul_status = rtc_get_status(RTC);
+
+	/*
+	*  Verifica por qual motivo entrou
+	*  na interrupcao, se foi por segundo
+	*  ou Alarm
+	*/
+	if ((ul_status & RTC_SR_SEC) == RTC_SR_SEC) {
+		//
+		//  Entrou por segundo!
+		//
+		rtc_clear_status(RTC, RTC_SCCR_SECCLR);
+		
+		segundo = 1;
+	}
+	
+	/* Time or date alarm */
+	if ((ul_status & RTC_SR_ALARM) == RTC_SR_ALARM) {
+			rtc_clear_status(RTC, RTC_SCCR_ALRCLR);
+      flag_led = 1;
+	}
+	
+	rtc_clear_status(RTC, RTC_SCCR_ACKCLR);
+	rtc_clear_status(RTC, RTC_SCCR_TIMCLR);
+	rtc_clear_status(RTC, RTC_SCCR_CALCLR);
+	rtc_clear_status(RTC, RTC_SCCR_TDERRCLR);
 }
 
 /************************************************************************/
@@ -151,44 +168,52 @@ void RTT_Handler(void)
 /*
  * @Brief Pisca LED placa
  */
-void pisca_led_tc(int n, int t){
+void pisca_led(int n, int t){
   for (int i=0;i<n;i++){
-		pio_clear(LED_PIO, LED_IDX_MASK);
-		delay_ms(t);
-		pio_set(LED_PIO, LED_IDX_MASK);
-		delay_ms(t);
+    pio_clear(LED_PIO, LED_IDX_MASK);
+    delay_ms(t);
+    pio_set(LED_PIO, LED_IDX_MASK);
+    delay_ms(t);
   }
 }
 
-void pisca_led_tc_2(int n, int t){
-	for (int i=0;i<n;i++){
-		pio_clear(LED1_PLACA_PIO, LED1_PLACA_IDX_MASK);
-		delay_ms(t);
-		pio_set(LED1_PLACA_PIO, LED1_PLACA_IDX_MASK);
-		delay_ms(t);
-	}
-}
-
-void pisca_led_rtc(int n, int t){
-	for (int i=0;i<n;i++){
-		pio_clear(LED3_PLACA_PIO, LED3_PLACA_IDX_MASK);
-		delay_ms(t);
-		pio_set(LED3_PLACA_PIO, LED3_PLACA_IDX_MASK);
-		delay_ms(t);
-	}
-}
-
 void pin_toggle(Pio *pio, uint32_t mask){
-	if(pio_get_output_data_status(pio, mask))
-	pio_clear(pio, mask);
-	else
-	pio_set(pio,mask);
+	if(pio_get_output_data_status(pio, mask)){
+		pio_clear(pio, mask);
+		pio_set(pio,mask);
+		pio_clear(pio, mask);
+	}else{
+		pio_set(pio,mask);
+		pio_clear(pio, mask);
+		pio_set(pio,mask);
+	}
+	
 }
 
 static float get_time_rtt(){
 	uint ul_previous_time = rtt_read_timer_value(RTT);
 }
 
+static void RTT_init(uint16_t pllPreScale, uint32_t IrqNPulses)
+{
+	uint32_t ul_previous_time;
+
+	/* Configure RTT for a 1 second tick interrupt */
+	rtt_sel_source(RTT, false);
+	rtt_init(RTT, pllPreScale);
+	
+	ul_previous_time = rtt_read_timer_value(RTT);
+	while (ul_previous_time == rtt_read_timer_value(RTT));
+	
+	rtt_write_alarm_time(RTT, IrqNPulses+ul_previous_time);
+
+	/* Enable RTT interrupt */
+	NVIC_DisableIRQ(RTT_IRQn);
+	NVIC_ClearPendingIRQ(RTT_IRQn);
+	NVIC_SetPriority(RTT_IRQn, 4);
+	NVIC_EnableIRQ(RTT_IRQn);
+	rtt_enable_interrupt(RTT, RTT_MR_ALMIEN | RTT_MR_RTTINCIEN);
+}
 
 /**
 * @Brief Inicializa o pino do LED
@@ -196,17 +221,20 @@ static float get_time_rtt(){
 void LED_init(int estado){
 	pmc_enable_periph_clk(LED_PIO_ID);
 	pio_set_output(LED_PIO, LED_IDX_MASK, estado, 0, 0);
+	pio_set(LED_PIO, LED_IDX_MASK);
 	
 	pmc_enable_periph_clk(LED1_PLACA_PIO_ID);
 	pio_set_output(LED1_PLACA_PIO, LED1_PLACA_IDX_MASK, estado, 0, 0);
+	pio_set(LED1_PLACA_PIO, LED1_PLACA_IDX_MASK);
 	
 	pmc_enable_periph_clk(LED2_PLACA_PIO_ID);
 	pio_set_output(LED2_PLACA_PIO, LED2_PLACA_IDX_MASK, estado, 0, 0);
-		
+	pio_set(LED2_PLACA_PIO, LED2_PLACA_IDX_MASK);
+	
 	pmc_enable_periph_clk(LED3_PLACA_PIO_ID);
 	pio_set_output(LED3_PLACA_PIO, LED3_PLACA_IDX_MASK, estado, 0, 0);
+	pio_set(LED3_PLACA_PIO, LED3_PLACA_IDX_MASK);
 };
-
 
 /**
 * Configura TimerCounter (TC) para gerar uma interrupcao no canal (ID_TC e TC_CHANNEL)
@@ -233,6 +261,7 @@ void TC_init(Tc * TC, int ID_TC, int TC_CHANNEL, int freq){
 
 	/* Configura e ativa interrupçcão no TC canal 0 */
 	/* Interrupção no C */
+  NVIC_SetPriority(ID_TC, 4);
 	NVIC_EnableIRQ((IRQn_Type) ID_TC);
 	tc_enable_interrupt(TC, TC_CHANNEL, TC_IER_CPCS);
 
@@ -240,34 +269,9 @@ void TC_init(Tc * TC, int ID_TC, int TC_CHANNEL, int freq){
 	tc_start(TC, TC_CHANNEL);
 }
 
-void TC_init_2(Tc * TC, int ID_TC, int TC_CHANNEL, int freq){
-	uint32_t ul_div;
-	uint32_t ul_tcclks;
-	uint32_t ul_sysclk = sysclk_get_cpu_hz();
-
-	/* Configura o PMC */
-	/* O TimerCounter é meio confuso
-	o uC possui 3 TCs, cada TC possui 3 canais
-	TC0 : ID_TC0, ID_TC1, ID_TC2
-	TC1 : ID_TC3, ID_TC4, ID_TC5
-	TC2 : ID_TC6, ID_TC7, ID_TC8
-	*/
-	pmc_enable_periph_clk(ID_TC);
-
-	/** Configura o TC para operar em  4Mhz e interrupçcão no RC compare */
-	tc_find_mck_divisor(freq, ul_sysclk, &ul_div, &ul_tcclks, ul_sysclk);
-	tc_init(TC, TC_CHANNEL, ul_tcclks | TC_CMR_CPCTRG);
-	tc_write_rc(TC, TC_CHANNEL, (ul_sysclk / ul_div) / freq);
-
-	/* Configura e ativa interrupçcão no TC canal 0 */
-	/* Interrupção no C */
-	NVIC_EnableIRQ((IRQn_Type) ID_TC);
-	tc_enable_interrupt(TC, TC_CHANNEL, TC_IER_CPCS);
-
-	/* Inicializa o canal 0 do TC */
-	tc_start(TC, TC_CHANNEL);
-}
-
+/**
+* Configura o RTC para funcionar com interrupcao de alarme
+*/
 void RTC_init(Rtc *rtc, uint32_t id_rtc, calendar t, uint32_t irq_type){
 	/* Configura o PMC */
 	pmc_enable_periph_clk(ID_RTC);
@@ -282,91 +286,66 @@ void RTC_init(Rtc *rtc, uint32_t id_rtc, calendar t, uint32_t irq_type){
 	/* Configure RTC interrupts */
 	NVIC_DisableIRQ(id_rtc);
 	NVIC_ClearPendingIRQ(id_rtc);
-	NVIC_SetPriority(id_rtc, 0);
+	NVIC_SetPriority(id_rtc, 4);
 	NVIC_EnableIRQ(id_rtc);
 
 	/* Ativa interrupcao via alarme */
-	rtc_enable_interrupt(rtc,  irq_type);
-}
-
-static void RTT_init(uint16_t pllPreScale, uint32_t IrqNPulses)
-{
-	uint32_t ul_previous_time;
-
-	/* Configure RTT for a 1 second tick interrupt */
-	rtt_sel_source(RTT, false);
-	rtt_init(RTT, pllPreScale);
-	
-	ul_previous_time = rtt_read_timer_value(RTT);
-	while (ul_previous_time == rtt_read_timer_value(RTT));
-	
-	rtt_write_alarm_time(RTT, IrqNPulses+ul_previous_time);
-
-	/* Enable RTT interrupt */
-	NVIC_DisableIRQ(RTT_IRQn);
-	NVIC_ClearPendingIRQ(RTT_IRQn);
-	NVIC_SetPriority(RTT_IRQn, 0);
-	NVIC_EnableIRQ(RTT_IRQn);
-	rtt_enable_interrupt(RTT, RTT_MR_ALMIEN);
+	rtc_enable_interrupt(rtc, irq_type);
 }
 
 /************************************************************************/
 /* Main Code	                                                        */
 /************************************************************************/
-
-int main (void)
-{
+int main(void){
+	/* Initialize the SAM system */
 	board_init();
 	sysclk_init();
 	delay_init();
 	
+	// Inicializa RTT com IRQ no alarme.
+	f_rtt_alarme = true;
+	
+	// Init OLED
+	gfx_mono_ssd1306_init();	
+	// Escreve na tela um circulo e um texto
+	// gfx_mono_draw_filled_circle(20, 16, 16, GFX_PIXEL_SET, GFX_WHOLE);
+	// gfx_mono_draw_string("mundo", 50,16, &sysfont);
+	
+	rtc_get_time(RTC, &pul_hour, &pul_minute, &pul_second);
+	sprintf(str, "%d:%d:%d", pul_hour, pul_minute, pul_second);
+	gfx_mono_draw_string(str, 0,16, &sysfont);
+
 	/* Disable the watchdog */
 	WDT->WDT_MR = WDT_MR_WDDIS;
-	
+
 	/* Configura Leds */
 	LED_init(0);
 
-  // Init OLED
-	gfx_mono_ssd1306_init();
-  
-  // Escreve na tela um circulo e um texto
-	gfx_mono_draw_filled_circle(20, 16, 16, GFX_PIXEL_SET, GFX_WHOLE);
-	
-	gfx_mono_draw_string("mundo", 50,16, &sysfont);
-	
+  /** Configura timer TC0, canal 1 */
 	TC_init(TC0, ID_TC1, 1, 4);
+	TC_init(TC1, ID_TC4, 1, 5);
 	
-	TC_init_2(TC0, ID_TC1, 2, 5);
-	
+	/** Configura RTC */
 	calendar rtc_initial = {2018, 3, 19, 12, 15, 45 ,1};
-	RTC_init(RTC, ID_RTC, rtc_initial, RTC_IER_ALREN);
-	
+	RTC_init(RTC, ID_RTC, rtc_initial, RTC_IER_ALREN | RTC_IER_SECEN);
+
+	/* configura alarme do RTC */
+	int tempo = 5;
 	rtc_set_date_alarm(RTC, 1, rtc_initial.month, 1, rtc_initial.day);
-	rtc_set_time_alarm(RTC, 1, rtc_initial.hour, 1, rtc_initial.minute, 1, rtc_initial.seccond + 20);
+	rtc_set_time_alarm(RTC, 1, rtc_initial.hour, 1, rtc_initial.minute, 1, rtc_initial.seccond + tempo);
+  
+	while (1)	{
+    if(flag_lad1){
+      pin_toggle(LED1_PLACA_PIO, LED1_PLACA_IDX_MASK);
+      flag_lad1 = 0;
+    }
 	
-	f_rtt_alarme = true;
-
-  /* Insert application code here, after the board has been initialized. */
-	while(1) {
-		if(flag_tc){
-			
-			pisca_led_tc_2(1,10);
-			flag_tc = 0;
-		}
-		
-		else if(flag_tc_2){
-			pisca_led_tc(1,10);
-			flag_tc_2 = 0;
-		}
-		
-		else if (flag_rtc){
-
-			pisca_led_rtc(5, 200);
-			
-			flag_rtc = 0;
-		}
-		
-		else if (f_rtt_alarme){
+	if(flag_lad2){
+		pin_toggle(LED2_PLACA_PIO, LED2_PLACA_IDX_MASK);
+		flag_lad2 = 0;
+	}
+	
+	if (f_rtt_alarme){
       
       /*
        * IRQ apos 4s -> 8*0.5
@@ -375,9 +354,37 @@ int main (void)
       uint32_t irqRTTvalue = 8;
       
       // reinicia RTT para gerar um novo IRQ
-      RTT_init(pllPreScale, irqRTTvalue);         
+      RTT_init(pllPreScale, irqRTTvalue);
+	  
+	  pin_toggle(LED3_PLACA_PIO, LED3_PLACA_IDX_MASK);     
       
       f_rtt_alarme = false;
     }
-  }
+	
+	if(flag_led){
+		for(int i = 0; i < 1; i++){
+			pisca_led(5, 100);
+		}
+		
+		tempo += 10;
+		
+		// RTC_init(RTC, ID_RTC, rtc_initial, RTC_IER_ALREN);
+		rtc_set_date_alarm(RTC, 1, rtc_initial.month, 1, rtc_initial.day);
+		rtc_set_time_alarm(RTC, 1, rtc_initial.hour, 1, rtc_initial.minute, 1, rtc_initial.seccond + tempo);
+		
+		flag_led = 0;
+	}
+	
+	if(segundo){
+		rtc_get_time(RTC, &pul_hour, &pul_minute, &pul_second);
+		sprintf(str, "%d:%d:%d", pul_hour, pul_minute, pul_second);
+		gfx_mono_draw_string(str, 0,16, &sysfont);
+		
+		segundo = 0;
+	}
+	
+    pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
+	
+	}
+	return 0;
 }
--- TC-RTC-RTT/OLED-Xplained-Pro-SPI/OLED-Xplained-Pro-SPI/src/oled/const.h
+++
@@ -1,7 +0,0 @@
-
-/*
- * const.h
- *
- * Created: 27/04/2020 22:10:11
- *  Author: Samuel Porto
- */ 
